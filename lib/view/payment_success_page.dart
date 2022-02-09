0f057da5ca7e800d6a1e4ef0407b8b5a130f4b26
yuchenwzt <43514944+yuchenwzt@users.noreply.github.com>
2022-02-09 17:28:34 -0500

    payment_success_after clicking the place order

---
+++ lib/view/payment_success_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'item/Item_page.dart';
+
+class PaymentPage extends StatelessWidget {
+  const PaymentPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 140),
+              child: Icon(Icons.done_outline, size: 120, color: Colors.green),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 100)),
+            const Center(
+                child: Text('Order has been placed successfully!',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+            const Padding(padding: EdgeInsets.only(top: 80)),
+            SizedBox(
+                width: 30,
+                height: 30,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: Material(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(5),
+                    elevation: 6,
+                    child: MaterialButton(
+                      child: const Text(
+                        'Back to Main Page',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                                builder: (context) => const ItemPage()),
+                            (route) => route == null);
+                      },
+                    ),
+                  ),
+                )),
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            SizedBox(
+                width: 30,
+                height: 30,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  child: Material(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(5),
+                    elevation: 6,
+                    child: MaterialButton(
+                      child: const Text(
+                        'View Orders',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ))
+          ],
+        ));
+  }
+  // SizedBox(height: getProportionateScreenWidth(20)),
+}
